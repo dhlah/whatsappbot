e1c0c1860fb4bbe7cd9336137cafd7ca6ce3f5c7
dhlah <aminahnaf03@gmail.com>
2024-01-26 21:49:01 +0700

    fix serverless function

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,36 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
-const fs = require('fs')
 
-const fileStatus = "./src/lib/status.json";
-function changeStatus(status) {
-  fs.readFile(fileStatus, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error membaca file:", err);
-      return;
-    }
-
-    // Mengonversi isi file menjadi objek JavaScript
-    const jsonData = JSON.parse(data);
-
-    // Mengubah nilai umur
-    jsonData.DatabaseConnect = status;
-
-    // Mengonversi objek JavaScript kembali ke string JSON
-    const jsonStringUpdated = JSON.stringify(jsonData, null, 2);
-
-    // Menyimpan kembali hasil ke file JSON
-    fs.writeFile(fileStatus, jsonStringUpdated, "utf8", (err) => {
-      if (err) {
-        console.error("Error menyimpan file:", err);
-        return;
-      }
-      console.log("🟢 Database Sudah Terhubung");
-    });
-  });
-}
 
 const prismaClientSingleton = () => {
-  changeStatus(true)
   return new PrismaClient();
 };
 
